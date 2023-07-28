76c94116e8b6d27d470c5bb88fc357116c432c28
Juan Felipe Rojas Muñoz <juanferojas19@gmail.com>
2023-07-28 08:00:35 -0500

    primera parte

---
+++ rock.py
@@ -0,0 +1,51 @@
+import random
+option=('rock', 'paper', 'scissors')
+win_user=0
+win_computer=0
+rounds=0
+
+while True:
+
+    rounds+=1
+    print('rounds: ', rounds )
+
+    user_option=input('choose option: ')
+    comput_option=random.choices(option)
+
+
+    if user_option==comput_option:
+        print('empate')
+    elif user_option=='rock':
+        if comput_option=='scissors':
+            win_user+=1
+        else:
+            win_computer+=1
+
+    elif user_option=='paper':
+        if comput_option=='rock':
+            win_user+=1
+        else:
+            win_computer+=1
+
+    elif user_option=='scissors':
+        if comput_option=='paper':
+            win_user+=1
+        else:
+            win_computer+=1
+    
+    
+    print('win_user: ', win_user)
+    print('win_computer: ', win_computer )
+
+    if win_computer==2:
+        print('win computer game')
+        break
+
+    if win_user==2:
+        print('win user game')
+        break
+
+
+
+
+
