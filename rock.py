01353069845f53a0a61e9e251348adfc5b3c1a20
Juan Felipe Rojas Muñoz <juanferojas19@gmail.com>
2023-07-31 07:50:52 -0500

    2 test

--- rock.py
+++ rock.py
@@ -6,19 +6,19 @@ rounds=0
 
 while True:
 
-    rounds+=1
     print('rounds: ', rounds )
 
     print('win_user: ', win_user)
     print('win_computer: ', win_computer )
 
     user_option=input('choose option: ')
+    rounds+=1
 
     if not user_option in option:
-        print()
+        print('nombre mal digitado')
         continue
 
-    comput_option=random.choices(option)
+    comput_option=random.choice(option)
 
 
     if user_option==comput_option:
@@ -41,12 +41,11 @@ while True:
         else:
             win_computer+=1
     
-    
-    if win_user==2:
+    if win_user==4:
         print('win user game')
         break
 
-    if win_computer==2:
+    if win_computer==4:
         print('win computer game')
         break
 
