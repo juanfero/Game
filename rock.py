0b2d73462d6043b8861d82b9446d1d4e38897cba
Juan Felipe Rojas Muñoz <juanferojas19@gmail.com>
2023-08-13 22:09:51 -0500

    funciones aplicadas

--- rock.py
+++ rock.py
@@ -1,18 +1,13 @@
 import random
-option=('rock', 'paper', 'scissors')
-win_user=0
-win_computer=0
-rounds=0
-
-while True:
-
-    rounds+=1
-    print('rounds: ', rounds )
-
+def opciones():
+    option=('rock', 'paper', 'scissors')
     user_option=input('choose option: ')
     comput_option=random.choices(option)
+    print('User option =>', user_option)
+    print('Computer option =>', comput_option)
+    return user_option,comput_option
 
-
+def rules(user_option,comput_option,win_user, win_computer):
     if user_option==comput_option:
         print('empate')
     elif user_option=='rock':
@@ -32,20 +27,32 @@ while True:
             win_user+=1
         else:
             win_computer+=1
+    return win_user,win_computer
     
-    
-    print('win_user: ', win_user)
-    print('win_computer: ', win_computer )
+def run():
 
-    if win_computer==2:
-        print('win computer game')
-        break
+    win_user=0
+    win_computer=0
+    rounds=0
 
-    if win_user==2:
-        print('win user game')
-        break
+    while True:
+        print('*' * 10)
+        print('ROUND', rounds)
+        print('*' * 10)
+        print('computer_wins', win_computer)
+        print('user_wins', win_user)
+        rounds+=1
+        print('rounds: ', rounds )
 
+        user_option, comput_option = opciones()
+        win_user, win_computer = rules(user_option, comput_option, win_user, win_computer)
 
+        if win_computer==2:
+            print('win computer game')
+            break
 
-
+        if win_user==2:
+            print('win user game')
+            break
+run()
 
